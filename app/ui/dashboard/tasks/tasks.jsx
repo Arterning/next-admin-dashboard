e1aa827f4fd5fe1e498de14eb9b606b9390557c9
Arterning <ning.huang.ridson@gmail.com>
2023-12-29 17:18:46 +0800

    task dashboard

--- app/ui/dashboard/tasks/tasks.jsx
+++ app/ui/dashboard/tasks/tasks.jsx
@@ -2,7 +2,9 @@
 
 import fetcher from '@/app/lib/fetcher';
 import { useQuery } from '@tanstack/react-query';
+import styles from "./tasks.module.css";
 import React from 'react';
+import { MdSupervisedUserCircle } from 'react-icons/md';
 export const LatestTasks = () => {
 
     const { data, error, isLoading } = useQuery({
@@ -15,15 +17,31 @@ export const LatestTasks = () => {
     if (error) return 'An error has occurred: ' + error.message
 
     return (
-      <div>
-          Here is LatestTasks Page
+      <div className={styles.container}>
+        <h2 className={styles.title}>Latest Tasks</h2>
+        <div className={styles.taskCards}>
           {
             data.tasks.map((task) => (
-              <div key={task.id}>
-                {task.title}
-              </div>
+              // must provide key
+              <TaskCard key={task.id} task={task}/>
             ))
           }
+        </div>
       </div>
     );
 };
+
+const TaskCard = ({ task }) => {
+  return (
+      <div className={styles.taskCard}>
+        <MdSupervisedUserCircle size={24} />
+        <div className={styles.taskCardText}>
+          <span className={styles.taskCardTitle}>{task.title}</span>
+          <span className={styles.taskCardCenter}>{task.description}</span>
+          <span className={styles.taskCardDetail}>
+            Detail
+          </span>
+        </div>
+      </div>
+  );
+}
